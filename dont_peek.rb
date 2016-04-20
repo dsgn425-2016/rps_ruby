7af584b5e2031e6de3cc90b6731a9493a5ad2123
Arjun Venkataswamy <avenkat2@gmail.com>
2016-04-19 22:36:53 -0500

    extends solution in class

--- dont_peek.rb
+++ dont_peek.rb
@@ -1,29 +1,41 @@
 # Rock, Paper, Scissors: Example solution for Version 1
 
 puts "Welcome to Rock, Paper, Scissors!"
+keep_going = ""
+while keep_going != "quit"
+  puts "rock, paper, or scissors?"
 
-puts "rock, paper, or scissors?"
+  valid_moves = ["rock", "paper", "scissors"]
+  user_move = gets.chomp.downcase
 
-user_move = gets.chomp
+  while valid_moves.include?(user_move) != true
+    puts "That doesn't seem to be a valid move."
+    puts "rock, paper, or scissors?"
+    user_move = gets.chomp.downcase
+  end
 
-puts "You chose #{user_move}."
+  puts "You chose #{user_move}."
 
-computer_move = ["rock", "paper", "scissors"].sample
+  computer_move = valid_moves.sample
 
-puts "The computer chose #{computer_move}."
+  puts "The computer chose #{computer_move}."
 
-if user_move == computer_move
-  puts "You tied!"
-elsif user_move == "paper" && computer_move == "rock"
-  puts "You win!"
-elsif user_move == "paper" && computer_move == "scissors"
-  puts "You lose!"
-elsif user_move == "scissors" && computer_move == "rock"
-  puts "You lose!"
-elsif user_move == "scissors" && computer_move == "paper"
-  puts "You win!"
-elsif user_move == "rock" && computer_move == "paper"
-  puts "You lose!"
-elsif user_move == "rock" && computer_move == "scissors"
-  puts "You win!"
+  if user_move == computer_move
+    puts "You tied!"
+  elsif user_move == "paper" && computer_move == "rock"
+    puts "You win!"
+  elsif user_move == "paper" && computer_move == "scissors"
+    puts "You lose!"
+  elsif user_move == "scissors" && computer_move == "rock"
+    puts "You lose!"
+  elsif user_move == "scissors" && computer_move == "paper"
+    puts "You win!"
+  elsif user_move == "rock" && computer_move == "paper"
+    puts "You lose!"
+  elsif user_move == "rock" && computer_move == "scissors"
+    puts "You win!"
+  end
+  puts "Do you want to quit? Type quit if you do, otherwise just hit enter."
+  keep_going = gets.chomp.downcase
 end
+puts "Thanks for playing"
--- rps.rb
+++ rps.rb
@@ -1,1 +1,20 @@
 # Rock, Paper, Scissors
+# puts "What's your fav animal????"
+# fav_animal = gets.chomp
+# puts "What's your fav color????"
+# fav_color = gets.chomp
+# puts "Your favorite animal is a(n) #{fav_animal}. Your favorite color is #{fav_color}"
+
+
+# animals = ["zebra", "sloth", "hyena", "dog"]
+# puts animals[1]  # => "sloth"
+# puts animals.sample
+
+animal = "cat"
+color = "yellow"
+
+if animal == "cat" && color == "yellow"
+  puts "You've got a yellow cat"
+elsif animal == "cat"
+  puts "You've got a cat"
+end
