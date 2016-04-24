be55dcb63229a04ab5f78f4308c866403c7d3812
FamaLJ <blongjohn2017@kellogg.northwestern.edu>
2016-04-24 07:01:38 -0500

    edit

--- rps.rb
+++ rps.rb
@@ -1,1 +1,33 @@
 # Rock, Paper, Scissors
+
+puts "Welcome to Rock, Paper, Scissors!"
+
+puts "rock, paper, or scissors?"
+
+user_move = ""
+
+while user_move != "quit"
+
+  user_move = gets.chomp
+
+  puts "You chose #{user_move}."
+
+  computer_move = ["rock", "paper", "scissors"].sample
+
+  puts "The computer chose #{computer_move}."
+
+  if user_move == computer_move
+    puts "You tied!"
+  elsif user_move == "paper" && computer_move == "rock"
+    puts "You win!"
+  elsif user_move == "paper" && computer_move == "scissors"
+    puts "You lose!"
+  elsif user_move == "scissors" && computer_move == "rock"
+    puts "You lose!"
+  elsif user_move == "scissors" && computer_move == "paper"
+    puts "You win!"
+  elsif user_move == "rock" && computer_move == "paper"
+    puts "You lose!"
+  elsif user_move == "rock" && computer_move == "scissors"
+    puts "You win!"
+  end
